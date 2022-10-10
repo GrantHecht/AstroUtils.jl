df51ee728ba21f934ee18c7d14e4db7e24077b21
Documenter.jl <documenter@juliadocs.github.io>
2022-10-10 15:24:59 +0000

    build based on cabb910

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AstroUtils","category":"page"},{"location":"#AstroUtils","page":"Home","title":"AstroUtils","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AstroUtils.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AstroUtils]","category":"page"}]
+}
